c3fbff88989c043214e45fa922caec9309bf2fed
GEONU-MOON <moondy2209@naver.com>
2024-07-29 18:47:24 +0900

    #2 refactor : 로그인 컴포넌트 분리 및 ui 개선

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const app = document.getElementById("app");
   app.innerHTML = `
-        <h1>Welcome to My SPA</h1>
         <nav>
             <a href="#home">Home</a>
-            <a href="#about">About</a>
-            <a href="#contact">Contact</a>
             <a href="#login">Login</a>
         </nav>
         <div id="content"></div>
@@ -13,19 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const routes = {
     home: "<h2>Home Page</h2><p>This is the home page content.</p>",
-    about: "<h2>About Page</h2><p>This is the about page content.</p>",
-    contact: "<h2>Contact Page</h2><p>This is the contact page content.</p>",
-    login: `
-      <form id="loginForm">
-          <img src="/images/Doori-world.png">
-          <h2>Doori World</h2>
-          <label for="userId">이메일 아이디 *</label>
-          <input type="text" id="userId" name="userId" placeholder="이메일 아이디" required />
-          <label for="password">비밀번호 *</label>
-          <input type="password" id="password" name="password" placeholder="비밀번호" required />
-          <button type="submit">구경하러 가기</button>
-      </form>
-    `,
+    login: createLoginForm(),
   };
 
   window.addEventListener("hashchange", () => {
