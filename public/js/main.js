89a3cc96a88243085b3ddf33ceb0f876beb4fd6f
hyeong1 <mi75625@gmail.com>
2024-07-29 19:14:47 +0900

    feat: history 객체로 라우팅, 동적 라우팅

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,57 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const app = document.getElementById("app");
-  app.innerHTML = `
-        <h1>Welcome to My SPA</h1>
-        <nav>
-            <a href="#home">Home</a>
-            <a href="#about">About</a>
-            <a href="#contact">Contact</a>
-        </nav>
-        <div id="content"></div>
-    `;
+const navigateTo = (url) => {
+  history.pushState(null, null, url);
+  router();
+};
 
-  const routes = {
-    home: "<h2>Home Page</h2><p>This is the home page content.</p>",
-    about: "<h2>About Page</h2><p>This is the about page content.</p>",
-    contact: "<h2>Contact Page</h2><p>This is the contact page content.</p>",
-  };
+const routes = [
+  { path: "/", view: () => "<h1>Home</h1>" },
+  { path: "/login", view: () => "<h1>Login</h1>" },
+  { path: "/diary/post", view: () => "<h1>Diary Post</h1>" },
+  { path: "/diary/:date", view: (params) => `<h1>Diary ${params.date}</h1>` },
+  { path: "/photo", view: () => "<h1>Photo</h1>" },
+  { path: "/photo/post", view: () => "<h1>Photo Post</h1>" },
+  { path: "/visitor", view: () => "<h1>Visitor</h1>" },
+];
 
-  window.addEventListener("hashchange", () => {
-    const hash = location.hash.substring(1);
-    const content = routes[hash] || "<h2>404</h2><p>Page not found</p>";
-    document.getElementById("content").innerHTML = content;
+const router = () => {
+  const potentialMatches = routes.map((route) => {
+    return {
+      route: route,
+      result: location.pathname.match(pathToRegex(route.path)),
+    };
   });
 
-  window.dispatchEvent(new HashChangeEvent("hashchange"));
+  let matchRoute = potentialMatches.find(
+    (potentialMatch) => potentialMatch.result !== null
+  );
+
+  if (!matchRoute) {
+    document.querySelector("#app").innerHTML = `<h1>404 not found</h1>`;
+    return;
+  }
+
+  const view = matchRoute.route.view(getParams(matchRoute));
+  document.querySelector("#app").innerHTML = view;
+};
+
+const pathToRegex = (path) =>
+  new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+
+const getParams = (matchRoute) => {
+  const values = matchRoute.result.slice(1); // 실제 url 에서 date 영역 가져오기
+  const keys = Array.from(matchRoute.route.path.matchAll(/:(\w+)/g)).map(
+    (result) => result[1]
+  ); // 동적 라우팅 ":date" 에서 "date" 가져오기
+  return Object.fromEntries(keys.map((key, i) => [key, values[i]])); // result ex: { date: "20240729" }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", (e) => {
+    if (e.target.matches("[data-link]")) {
+      e.preventDefault(); // 새로고침 막기
+      navigateTo(e.target.href);
+    }
+  });
+
+  router();
 });
