c35715f4eab49438cd0c1d7e91a4316a085de4b2
GEONU-MOON <moondy2209@naver.com>
2024-07-29 16:39:08 +0900

    #2 feat : 로그인 라우팅 추가

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
             <a href="#home">Home</a>
             <a href="#about">About</a>
             <a href="#contact">Contact</a>
+            <a href="#login">Login</a>
         </nav>
         <div id="content"></div>
     `;
@@ -14,6 +15,17 @@ document.addEventListener("DOMContentLoaded", () => {
     home: "<h2>Home Page</h2><p>This is the home page content.</p>",
     about: "<h2>About Page</h2><p>This is the about page content.</p>",
     contact: "<h2>Contact Page</h2><p>This is the contact page content.</p>",
+    login: `
+      <form id="loginForm">
+          <img src="/images/Doori-world.png">
+          <h2>Doori World</h2>
+          <label for="userId">이메일 아이디 *</label>
+          <input type="text" id="userId" name="userId" placeholder="이메일 아이디" required />
+          <label for="password">비밀번호 *</label>
+          <input type="password" id="password" name="password" placeholder="비밀번호" required />
+          <button type="submit">구경하러 가기</button>
+      </form>
+    `,
   };
 
   window.addEventListener("hashchange", () => {
