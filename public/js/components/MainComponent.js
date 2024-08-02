069e4654eaa0d6fe9249fbb910d6abc4c974ff9f
GEONU-MOON <moondy2209@naver.com>
2024-08-03 00:36:29 +0900

    #6 Design : 메인 컨텐츠 영역 UI구현

--- public/js/components/MainComponent.js
+++ public/js/components/MainComponent.js
@@ -3,7 +3,21 @@ function MainComponent() {
     <div class="wrapper">
       <div class="wrapper-line">
         <div class="profile-wrapper"></div>
-        <div class="main-wrapper"></div>
+        <div class="main-wrapper">
+          <div class="header-box">
+            <div class="intro">열심히 개발ㅎrㅈr...S2</div>
+            <div class="outro">Hello, Doori World!</div>
+          </div>
+          <div class="content-area"> 
+            <div class="white-box"></div>
+            <div class="tab-container">
+              <div class="active-tab-item">홈</div>
+              <div class="tab-item">다이어리</div>
+              <div class="tab-item">포스트</div>
+              <div class="tab-item">방명록</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   `;
