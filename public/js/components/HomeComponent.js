d8e33ee7617aae9edcdfd967d8e07fccb8e049e5
GEONU-MOON <moondy2209@naver.com>
2024-08-05 20:22:00 +0900

    #11 refact: 더미데이터 활용하여 홈 화면을 구성

--- public/js/components/HomeComponent.js
+++ public/js/components/HomeComponent.js
@@ -8,47 +8,76 @@ function loadCSS() {
 
 function HomeComponent() {
   loadCSS();
+
+  const diaryContents = [
+    "다이어리 내용1",
+    "다이어리 내용2",
+    "다이어리 내용3",
+    "다이어리 내용4",
+  ];
+  const photos = [
+    "/resource/images/couple.png",
+    "/resource/images/nongmoong.png",
+    "/resource/images/Doori-world.png",
+    "/resource/images/miniroom.png",
+  ];
+  const miniroomImage = "/resource/images/mini.png";
+  const visitorComments = [
+    "방명록 내용 1",
+    "방명록 내용 2",
+    "방명록 내용 3",
+    "방명록 내용 4",
+  ];
+
+  const diaryHtml = diaryContents
+    .slice(0, 3)
+    .map((content) => `<div class="diary-item">${content}</div>`)
+    .join("");
+  const photosHtml = photos
+    .slice(0, 3)
+    .map((photo) => `<img src="${photo}">`)
+    .join("");
+  const visitorsHtml = visitorComments
+    .slice(0, 2)
+    .map((comment) => `<div class="visitor-comment-item">${comment}</div>`)
+    .join("");
+
   return ` 
-  <div class="home-container">
-    <div class="Diary-section">
-      <div class="Diary-title">
-        Diary<hr>
+    <div class="home-container">
+     <div class="top-section">
+        <div class="Diary-section">
+          <div class="Diary-title">
+            Diary
+          </div>
+          <div class="Diary-show"> 
+            ${diaryHtml}
+          </div>
+        </div>
+        <div class="photo-section">
+          <div class="photo-title">
+            Photo
+          </div>
+          <div class="photo-gallery">
+            ${photosHtml}
+          </div>
+        </div>
       </div>
-      <div class="Diary-show"> 
-        안녕하세요 저는 문건우입니다.</br>
-        <hr>
-        민형이의 일기
-        <hr>
+      <div class="miniroom-section">
+        <div class="miniroom-title">
+          Mini Room
+        </div>
+        <div class="miniroom-image">
+          <img src="${miniroomImage}" alt="Mini Room">
+        </div>
       </div>
-    </div>
-    <div class="photo-section">
-      <div class="photo-title">
-        Photo<hr>
-      </div>
-      <div class="photo-gallery">
-        <img src="/resource/images/couple.png">
-        <img src="/resource/images/nongmoong.png">
-        <img src="/resource/images/Doori-world.png">
-      </div>
-    </div>
-    <div class="miniroom-section">
-      <div class="miniroom-title">
-        Mini Room<hr>
-      </div>
-      <div class="miniroom-image">
-        <img src="/resource/images/mini.png" alt="Mini Room">
-      </div>
-    </div>
-    <div class="visitor-section">
-      <div class="visitor-title">
-        What Visitors Say<hr>
-      </div> 
-      <div class="visitor-comment">
-        뇽하~ (뭉)</br>
-        <hr>
-        건하~ (뇽)<hr>
+      <div class="visitor-section">
+        <div class="visitor-title">
+          What Visitors Say
+        </div> 
+        <div class="visitor-comment">
+          ${visitorsHtml}
+        </div>
       </div>
     </div>
-  </div>
-`;
+  `;
 }
