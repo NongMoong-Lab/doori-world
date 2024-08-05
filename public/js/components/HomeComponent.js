914b79a6c3c143f7d596b7f0c616cbf1770485eb
GEONU-MOON <moondy2209@naver.com>
2024-08-05 21:47:37 +0900

    #11 feat : 홈화면에서 diary와 방명록 타임스탬프 내림차순 정렬로 가장 최신 게시물 렌더링

--- public/js/components/HomeComponent.js
+++ public/js/components/HomeComponent.js
@@ -10,10 +10,26 @@ function HomeComponent() {
   loadCSS();
 
   const diaryContents = [
-    "다이어리 내용1",
-    "다이어리 내용2",
-    "다이어리 내용3",
-    "다이어리 내용4",
+    {
+      content: "다이어리 내용1",
+      author: "건우",
+      timestamp: "2024-08-05 14:00",
+    },
+    {
+      content: "다이어리 내용2",
+      author: "민형",
+      timestamp: "2024-08-07 12:20",
+    },
+    {
+      content: "다이어리 내용3",
+      author: "건우",
+      timestamp: "2024-08-08 18:45",
+    },
+    {
+      content: "다이어리 내용4",
+      author: "민형",
+      timestamp: "2024-08-06 15:30",
+    },
   ];
   const photos = [
     "/resource/images/couple.png",
@@ -23,23 +39,41 @@ function HomeComponent() {
   ];
   const miniroomImage = "/resource/images/mini.png";
   const visitorComments = [
-    "방명록 내용 1",
-    "방명록 내용 2",
-    "방명록 내용 3",
-    "방명록 내용 4",
+    { comment: "방명록 내용 1", timestamp: "2024-08-05 14:00" },
+    { comment: "방명록 내용 2", timestamp: "2024-08-06 15:30" },
+    { comment: "방명록 내용 3", timestamp: "2024-08-07 12:20" },
+    { comment: "방명록 내용 4", timestamp: "2024-08-08 18:45" },
   ];
 
   const diaryHtml = diaryContents
-    .slice(0, 3)
-    .map((content) => `<div class="diary-item">${content}</div>`)
+    .sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp)) // 최신 날짜가 먼저 오도록 내림차순 정렬
+    .slice(0, 3) // 상위 3개 항목만 렌더링
+    .map(
+      (item) => `
+    <div class="diary-item">
+      ${item.content}
+      <div class="diary-author">${item.author}</div>
+    </div>
+  `
+    )
     .join("");
+
   const photosHtml = photos
     .slice(0, 3)
     .map((photo) => `<img src="${photo}">`)
     .join("");
+
   const visitorsHtml = visitorComments
+    .sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp)) // 내림차순 정렬
     .slice(0, 2)
-    .map((comment) => `<div class="visitor-comment-item">${comment}</div>`)
+    .map(
+      (item) => `
+      <div class="visitor-comment-item">
+        <div class="comment-content">${item.comment}</div>
+        <div class="comment-timestamp">${item.timestamp}</div>
+      </div>
+    `
+    )
     .join("");
 
   return ` 
