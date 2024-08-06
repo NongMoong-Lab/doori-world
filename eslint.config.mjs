208e824eb585660df32dbff95a00c7df675a8f94
hyeong1 <mi75625@gmail.com>
2024-08-06 20:52:18 +0900

    update: eslint setting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
+// import pluginJs from "@eslint/js";
 
 export default [
   {
@@ -9,10 +9,10 @@ export default [
       "no-console": "warn", // 콘솔 로그 사용에 대해 경고
       indent: ["error", 2], // 들여쓰기를 2칸으로 설정
       quotes: ["error", "double"], // 문자열 쌍따옴표
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
+      // "no-unused-vars": "warn",
+      // "no-undef": "warn",
       "prefer-const": ["warn", { ignoreReadBeforeAssign: true }],
     },
   },
-  pluginJs.configs.recommended,
+  // pluginJs.configs.recommended,
 ];
