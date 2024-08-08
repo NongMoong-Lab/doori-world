14c6ad5e386a3c29b684919451cfc2419e81254c
hyeong1 <mi75625@gmail.com>
2024-08-08 22:44:51 +0900

    #25 feat: mongodb 연결
    Co-authored-by: 문건우 <GEONU-MOON@users.noreply.github.com>

---
+++ server/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const mongoURI = "mongodb+srv://nongmong0605:tnfIYc4ZzTZfejVZ@nongmong.t94afgm.mongodb.net/doori_world?retryWrites=true&w=majority&appName=nongmong";
+
+// MongoDB 연결
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("MongoDB에 연결되었습니다.");
+  } catch (err) {
+    console.error("MongoDB 연결 오류:", err);
+    process.exit(1); // 연결 실패 시 프로세스 종료
+  }
+};
+
+// 이 함수를 다른 파일에서 사용할 수 있도록 export
+module.exports = connectDB;
