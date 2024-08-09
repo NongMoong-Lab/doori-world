993adf73656bf4eb1d01307a7eaf30084cbe9220
hyeong1 <mi75625@gmail.com>
2024-08-09 16:47:10 +0900

    #27 feat: 로그인 성공 시 jwt 토큰 발급 및 요청 헤더에 추가
    Co-authored-by: 문건우 <GEONU-MOON@users.noreply.github.com>

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 router.post("/login", async (req, res) => {
   const { input_id, input_pw } = req.body;
@@ -17,9 +19,13 @@ router.post("/login", async (req, res) => {
     }
 
     // 비밀번호 일치시
-    res.status(200).send({ message: "Login successful" });
+    const token = jwt.sign({ userId: user.user_id, userAvatar: user.user_avatar }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.setHeader("Authorization", `Bearer ${token}`);
+    res.status(200).json({ message: "Login success" });
   } catch (error) {
-    res.status(500).send({ error: "에러" });
+    res.status(500).send({ error: error });
   }
 });
 
